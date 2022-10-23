1eb9b55c0a1cae09ab5c55b1e8d7703dce7df3d1
Amirzhan <dusenbaev101@gmail.com>
2022-10-23 18:11:55 +0400

    Start

---
+++ src/main/java/com/example/pizzaweblab/PizzaWebLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzaweblab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaWebLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaWebLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pizzaweblab/PizzaWebLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzaweblab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaWebLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
